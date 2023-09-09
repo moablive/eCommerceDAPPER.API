50496f948a3c522ee0949e145ab64e69477dc5d6
Moab <guilhermeferrazbonato@gmail.com>
2023-09-08 22:32:41 -0300

    update

--- eCommerceDAPPER.API/obj/Debug/net5.0/eCommerceDAPPER.API.AssemblyInfo.cs
+++ eCommerceDAPPER.API/obj/Debug/net5.0/eCommerceDAPPER.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eCommerceDAPPER.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6995fd97eef434dbdd3c42955b7be2d62e442baf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("eCommerceDAPPER.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eCommerceDAPPER.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
